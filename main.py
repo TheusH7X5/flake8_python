da4bb3b9cc0431c2dc104683bbfb3281132f3a4f
Matheus <matheusa7x.23@gmail.com>
2020-07-08 01:11:03 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fila_normal import FilaNormal
+from fila_prioritaria import FilaPrioritaria
+
+#fila_teste = FilaNormal()
+#fila_teste.atualizafila()
+#fila_teste.atualizafila()
+#fila_teste.atualizafila()
+#print(fila_teste.chamacliente(12))
+#print(fila_teste.chamacliente(13))
+
+fila_teste2 = FilaPrioritaria()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+print(fila_teste2.chama_cliente(14))
+print(fila_teste2.chama_cliente(15))
+print(fila_teste2.estatistica('10/01/1993', 198, 'detail'))
