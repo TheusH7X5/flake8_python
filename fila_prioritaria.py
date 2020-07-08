8b52a41ad8150430eb3fb70e4ac07ed737ef60c6
Matheus <matheusa7x.23@gmail.com>
2020-07-08 02:27:07 -0300

    MN -> PR in priority queue

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -4,7 +4,7 @@ from fila_base import FilaBase
 class FilaPrioritaria(FilaBase):
 
     def gera_senha_atual(self) -> None:
-        self.senha_atual = f'NM{self.codigo}'
+        self.senha_atual = f'PR{self.codigo}'
 
     def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
