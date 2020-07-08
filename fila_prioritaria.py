829f0e8e7747867ffa22944e9f77d7c6487f3b62
Matheus <matheusa7x.23@gmail.com>
2020-07-08 01:44:45 -0300

    creating fila_base

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,17 +1,10 @@
-class FilaPrioritaria:
-    codigo: int = 0
-    fila = []
-    clientes_atendidos = []
-    senha_atual: str = ''
+from fila_base import FilaBase
+
+
+class FilaPrioritaria(FilaBase):
 
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'NM{self.codigo}'
-
-    def reseta_fila(self) ->None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
 
     def atualiza_fila(self) -> None:
         self.reseta_fila()
@@ -31,6 +24,8 @@ class FilaPrioritaria:
             estatistica['dia'] = dia
             estatistica['agencia'] = agencia
             estatistica['clientes atendidos'] = self.clientes_atendidos
-            estatistica['quantidade clientes atendidos'] = len(self.clientes_atendidos)
+            estatistica['quantidade clientes atendidos'] = (
+                len(self.clientes_atendidos)
+            )
 
         return estatistica
